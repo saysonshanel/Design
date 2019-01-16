612ee7a0fcb4a9f6168df1497ab8fd4a718d6fa2
saysonshanel <saysonshanel@gmail.com>
2019-01-16 14:50:25 -0800

    Add files via upload

---
+++ Observer/Observer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverExample
+{
+    public class Observer : IObserver
+    {
+        public string ObserverName { get; private set; }
+        public Observer(string name)
+        {
+            this.ObserverName = name;
+        }
+        public void Update()
+        {
+            Console.WriteLine("{0}: A new product has arrived at the store",this.ObserverName);
+        }
+    }
+
+    interface IObserver
+    {
+        void Update();
+    }
+}
---
+++ Observer/Subject.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverExample
+{
+  
+public class Subject : ISubject
+    {
+        private List<Observer> observers = new List<Observer>();
+        private int _int;
+        public int Inventory
+        {
+            get
+            {
+                return _int;
+            }
+            set
+            {
+                // Just to make sure that if there is an increase in inventory then only we are notifying  the observers.
+          if (value > _int)
+                    Notify();
+                _int = value;
+            }
+        }
+        public void Subscribe(Observer observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void Unsubscribe(Observer observer)
+        {
+            observers.Remove(observer);
+        }
+
+        public void Notify()
+        {
+            observers.ForEach(x => x.Update());
+        }
+    }
+
+    interface ISubject
+    {
+        void Subscribe(Observer observer);
+        void Unsubscribe(Observer observer);
+        void Notify();
+    }
+}
